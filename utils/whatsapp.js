9503a93f80e4e171d82a95ae45125b0dc3264753
emereson <escanorrosa123@gmail.com>
2024-07-02 23:50:06 -0500

    hol

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -2,13 +2,13 @@ const { Client, NoAuth } = require('whatsapp-web.js');
 
 // Inicializar el cliente de WhatsApp
 const clientWhatsApp = new Client({
+  authStrategy: new NoAuth(),
   puppeteer: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
-  authStrategy: new NoAuth(),
   webVersionCache: {
     type: 'remote',
-    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html`,
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014631230-alpha.html`,
   },
 });
 
