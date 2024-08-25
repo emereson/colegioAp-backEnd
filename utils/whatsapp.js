2dd639a7d6432d01a12224033b7db176a6cd4580
Emereson <escanorrosa123@gmail.com>
2024-08-24 20:31:13 -0500

    hol

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -3,6 +3,7 @@ const { Client, NoAuth } = require('whatsapp-web.js');
 // Inicializar el cliente de WhatsApp
 const clientWhatsApp = new Client({
   authStrategy: new NoAuth(),
+
   puppeteer: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
