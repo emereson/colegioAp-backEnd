410168cd0eb2beed7b7a27bc8eab17d3849d103f
emereson <escanorrosa123@gmail.com>
2024-09-30 07:34:04 -0500

    ggg

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -1,20 +1,28 @@
 const { Client, NoAuth } = require('whatsapp-web.js');
+const puppeteer = require('puppeteer');
 
-// Inicializar el cliente de WhatsApp
 const clientWhatsApp = new Client({
   authStrategy: new NoAuth(),
-
   puppeteer: {
+    executablePath: puppeteer.executablePath(), // Usar el ejecutable integrado
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
 });
 
-clientWhatsApp.on('qr', (qr) => {
-  console.log(qr);
-});
-
+const obtenerQR = () => {
+  clientWhatsApp.on('qr', (qr) => {
+    console.log(qr);
+  });
+};
 clientWhatsApp.on('ready', () => {
   console.log('Client is ready!');
 });
+
+obtenerQR();
+
+// Ejecutar la función cada 6 horas (21,600,000 ms)
+setInterval(() => {
+  obtenerQR();
+}, 42600000);
 
 module.exports = { clientWhatsApp };
